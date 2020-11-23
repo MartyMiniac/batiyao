dfb52376d938374944288a4300e290fb75a30f0f
Rohan Verma <rohanvermaara@gmail.com>
2020-11-23 12:07:10 +0530

    some improvements made in api and html

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from scapy.all import ARP, Ether, srp
 import socket
 from flask import *
+import copy
 
 app=Flask('__main__')
 allclients=[]
+userinfo={}
 
 def getallip():
     myip=str(socket.gethostbyname(socket.gethostname()))
@@ -16,11 +18,40 @@ def getallip():
     clients = []
     for sent, received in result:
         clients.append({'ip': received.psrc, 'mac': received.hwsrc})
-    allclients=clients
+        allclients.append({'ip': received.psrc, 'mac': received.hwsrc})
     print("Available devices in the network:")
     print("IP" + " "*18+"MAC")
     for client in clients:
         print("{:16}    {}".format(client['ip'], client['mac']))
 
+#api routes
+@app.route('/api/login', methods=['POST'])
+def api_login():
+    userinfo['name']=request.form['name']
+    userinfo['nickname']=request.form['nickname']
+    response = make_response(redirect('/session'))
+    response.set_cookie('name', request.form['name'])
+    response.set_cookie('nicknamename', request.form['nickname'])
+    return response
+
+@app.route('/api/getuserinfo', methods=['GET'])
+def api_getuserinfo():
+    return jsonify(userinfo)
+
+@app.route('/api/getuserlist', methods=['GET'])
+def api_getuserlist():
+    getallip()
+    return jsonify(allclients)
+
+#html routes
+@app.route('/')
+def flask_index():
+    return render_template('index.html')
+
+@app.route('/session')
+def flask_session():
+    return render_template('session.html')
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', debug=True, port=80)
